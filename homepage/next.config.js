986333c894aed0224a8b45f7fffaad909a6f58a0
Bruno Silva <bruno3dcontato@gmail.com>
2023-03-03 10:48:31 -0300

    fix: solve mf inconsistencies

--- homepage/next.config.js
+++ homepage/next.config.js
@@ -1,18 +1,29 @@
 const NextFederationPlugin = require('@module-federation/nextjs-mf');
 
+// this enables you to use import() and the webpack parser
+// loading remotes on demand, not ideal for SSR
+const remotes = (isServer) => {
+  const location = isServer ? 'ssr' : 'chunks';
+
+  return {
+    components: `components@http://localhost:3001/_next/static/${location}/remoteEntry.js`,
+  };
+};
+
 module.exports = {
-  webpack(config, options) {
-    if (!options.isServer) {
-      config.plugins.push(
-        new NextFederationPlugin({
-          name: 'home',
-          filename: 'static/chunks/remoteEntry.js',
-          remotes: {
-            components: "components@http//localhost:3001/.next/static/chunks/remoteEntry.js"
-          }
-        }),
-      );
-    }
+  /**
+   *
+   * @param {import('webpack').Configuration} config
+   * @returns {import('webpack').Configuration}
+   */
+  webpack(config, { isServer }) {
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: 'home',
+        filename: 'static/chunks/remoteEntry.js',
+        remotes: remotes(isServer),
+      })
+    );
     return config;
   },
 };
